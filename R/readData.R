cdc5bd976eb4c2a3d74f71ac924291e54467f3f2
Jack Leary <jrleary@live.unc.edu>
2020-06-17 07:38:53 -0400

    added datasets, updated readData() documentation

--- R/readData.R
+++ R/readData.R
@@ -1,21 +1,19 @@
-#' Read HTS plate data
+#' Read .xlsx HTS data
 #'
-#' This function reads in a .xlsx file, and outputs the name of the file being read before returning it. Useful for keeping track of data in large for loops.
-#' @param parent.dir The directory within which the .xlsx file is located
+#' This function reads in a .xlsx file, and outputs the name of the file being read before returning it. Useful for keeping track of data in large `for` loops.
+#' @param parent.dir The directory within which the .xlsx file is located.
 #' @param file.name The .xlsx file you wish to load.
-#' @param col.names Should column names be loaded? Defaults to TRUE
+#' @param col.names Should column names be loaded? Defaults to TRUE.
 #' @import openxlsx
 #' @export
-#' @example
-#' readData(parent.dir = "path/to/data/dir/", file.name = "file.xlsx)
+#' @examples
+#' readData(parent.dir = "path/to/data/dir/", file.name = "file.xlsx")
 
 readData <- function(parent.dir, file.name, col.names = TRUE) {
   filepath <- sprintf("%s%s.xlsx",
                       parent.dir,
                       file.name)
-  sprintf("Reading file: %s",
-          file.name)
-  df <- read.xlsx(filepath,
-                  colNames = col.names)
+  sprintf("Reading file: %s", file.name)
+  df <- read.xlsx(filepath, colNames = col.names)
   return(df)
 }
