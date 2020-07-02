6710bb873148611e9eca644f306c9839ffa25d34
Maddy Jenner <jennerma@live.unc.edu>
2020-07-02 07:25:52 -0400

    function to calculate IC50

--- R/compute_IC50.R
+++ R/compute_IC50.R
@@ -1,22 +1,29 @@
+library(YehLabHTS)
+library("data.table")
+library(drexplorer)
+library(reshape2)
+library(ggplot2)
+library("tidyverse")
+
 calculateIC50 <- function(drug_results) {
   ic50_results <- data.frame()
-  #every library and anchor drug combination
-  pairs <- unique(paste(drug_results[[1]]$Compound, drug_results[[1]]$Anchor))
-  library_drugs <- unique(drug_results[[1]]$Compound)
-  anchor_drugs <- unique(drug_results[[1]]$Anchor)
-
-  for(h in 2:length(drug_results)) {
+  for(h in 1:length(drug_results)) {
     #anch drugs needs to start at 2 to avoid "anchor doses = 0"
     for (i in 2:length(anchor_drugs)) {
       for(j in 1:length(library_drugs)) {
+        #every library and anchor drug combination
+        pairs <- unique(paste(drug_results[[h]]$Compound, drug_results[[h]]$Anchor))
+        library_drugs <- unique(drug_results[[h]]$Compound)
+        anchor_drugs <- unique(drug_results[[h]]$Anchor)
+
         #makes a subset for each unique anchor/lib pair
-        subset <- drug_results[[h]][ which(drug_results[[h]]$Compound == library_drugs[[j]] & drug_results[[h]]$Anchor == anchor_drugs[[i]])]
-        subset <- setorder(subset, Dose, AnchorDose)
-        compound_dose <- unique(subset$Dose)
-        anchor_dose <- unique(subset$AnchorDose)
+        unique_pair_subset <- drug_results[[h]][ which(drug_results[[h]]$Compound == library_drugs[[j]] & drug_results[[h]]$Anchor == anchor_drugs[[i]])]
+        unique_pair_subset <- setorder(unique_pair_subset, Dose, AnchorDose)
+        compound_dose <- unique(unique_pair_subset$Dose)
+        anchor_dose <- unique(unique_pair_subset$AnchorDose)
 
         #matrix for one anchor/lib pair with compound doses along the y, anchor doses on the x
-        matrix <- matrix(as.numeric(subset$inhibition), length(anchor_dose), length(compound_dose), byrow = F)
+        viabilityMatrix <- matrix(as.numeric(unique_pair_subset$inhibition), length(anchor_dose), length(compound_dose), byrow = F)
 
         compound_dose <- append(compound_dose, 0)
         compound_dose <- sort(compound_dose)
@@ -26,16 +33,16 @@ calculateIC50 <- function(drug_results) {
         #add in a row to the matrix for the DMSO anchor dose
         zero_dose_anchor <- drug_results[[h]][ which(drug_results[[h]]$Compound == library_drugs[[j]] & drug_results[[h]]$Anchor == "Anchor doses = 0")]
         zero_dose_anchor <- zero_dose_anchor[order(Dose),]
-        matrix <- rbind(zero_dose_anchor$inhibition, matrix)
-        matrix <- cbind(0, matrix)
+        viabilityMatrix <- rbind(zero_dose_anchor$inhibition, viabilityMatrix)
+        viabilityMatrix <- cbind(0, viabilityMatrix)
 
-        colnames(matrix) <- compound_dose
-        rownames(matrix) <- anchor_dose
+        colnames(viabilityMatrix) <- compound_dose
+        rownames(viabilityMatrix) <- anchor_dose
 
         #calculates IC50 of lib compound only (anchor dose=0)
-        fit <- drFit(drMat = cbind(as.numeric(colnames(matrix)[c(1:7)]),1-matrix[1, c(1:7)]/100), modelName = "sigEmax", alpha=1, fitCtr=T)
+        fit <- drFit(drMat = cbind(as.numeric(colnames(viabilityMatrix)[c(1:7)]),1-viabilityMatrix[1, c(1:7)]/100), modelName = "sigEmax", alpha=1, fitCtr=T)
         ic50 <- as.numeric(computeIC(fit, log.d=FALSE, interpolation=F))
-        if(all(1-matrix[1,c(1:7)]/100 > 0.5)) ic50 = NA
+        if(all(1-viabilityMatrix[1,c(1:7)]/100 > 0.5)) ic50 = NA
         print(ic50)
 
         ic50_results <- rbind(ic50_results, c(anchor_drugs[[i]], library_drugs[[j]], ic50))
@@ -44,3 +51,25 @@ calculateIC50 <- function(drug_results) {
     }
   }
 }
+
+
+######plotting and visualizing IC50 ranges
+#for each file there are 2 of the same library drugs
+
+#total number of IC50 values calculated for 1 drug file (102)
+352 - sum(is.na(ic50_results$ic50))
+
+#range is 1.1 e-7 to Inf
+range(ic50_results$ic50, na.rm = T)
+
+#ic50Data is ic50 results with only numbers (removes Inf and NA)
+ic50Data <- ic50_results[!is.na(as.numeric(as.character(ic50_results$ic50))),]
+ic50Data <- ic50Data[!grepl("Inf", ic50Data$ic50),]
+ic50Data <- setorder(ic50Data, ic50)
+ic50Data <- as.numeric(ic50Data$ic50)
+ic50Data <- as.data.frame(ic50Data)
+
+hist(ic50Data, freq = T, xlim = c(0, 1e-05), breaks = 20)
+abline(v = median(ic50Data), col = "red", lwd = 2)
+
+
